2f2b1f5d3702da76b8c926150fecac1f38b01558
Markus Leutwyler <tomcat@inorbit.ch>
2021-02-12 11:20:39 +0100

    Create co2ampel_cloud_wlan.ino
    Initial commit

---
+++ code/co2ampel_cloud_wlan.ino
@@ -0,0 +1,366 @@
+/*
+read ppm from scd30 and display on neopixel strip (stick/ring)
+*/
+
+#include <Wire.h>
+
+//#include <Adafruit_Sensor.h>
+//#include "Adafruit_BME680.h"
+
+#include <Adafruit_NeoPixel.h>
+
+#include "SparkFun_SCD30_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_SCD30
+SCD30 airSensor;
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include <HTTPClient.h>
+
+#include <WiFiClientSecure.h>
+
+// This is GandiStandardSSLCA2.pem, the root Certificate Authority that signed 
+// the server certifcate for the demo server https://jigsaw.w3.org in this
+// example. This certificate is valid until Sep 11 23:59:59 2024 GMT
+const char* rootCACertificate = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIF6TCCA9GgAwIBAgIQBeTcO5Q4qzuFl8umoZhQ4zANBgkqhkiG9w0BAQwFADCB\n" \
+"iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl\n" \
+"cnNleSBDaXR5MR4wHAYDVQQKExVUaGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNV\n" \
+"BAMTJVVTRVJUcnVzdCBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTQw\n" \
+"OTEyMDAwMDAwWhcNMjQwOTExMjM1OTU5WjBfMQswCQYDVQQGEwJGUjEOMAwGA1UE\n" \
+"CBMFUGFyaXMxDjAMBgNVBAcTBVBhcmlzMQ4wDAYDVQQKEwVHYW5kaTEgMB4GA1UE\n" \
+"AxMXR2FuZGkgU3RhbmRhcmQgU1NMIENBIDIwggEiMA0GCSqGSIb3DQEBAQUAA4IB\n" \
+"DwAwggEKAoIBAQCUBC2meZV0/9UAPPWu2JSxKXzAjwsLibmCg5duNyj1ohrP0pIL\n" \
+"m6jTh5RzhBCf3DXLwi2SrCG5yzv8QMHBgyHwv/j2nPqcghDA0I5O5Q1MsJFckLSk\n" \
+"QFEW2uSEEi0FXKEfFxkkUap66uEHG4aNAXLy59SDIzme4OFMH2sio7QQZrDtgpbX\n" \
+"bmq08j+1QvzdirWrui0dOnWbMdw+naxb00ENbLAb9Tr1eeohovj0M1JLJC0epJmx\n" \
+"bUi8uBL+cnB89/sCdfSN3tbawKAyGlLfOGsuRTg/PwSWAP2h9KK71RfWJ3wbWFmV\n" \
+"XooS/ZyrgT5SKEhRhWvzkbKGPym1bgNi7tYFAgMBAAGjggF1MIIBcTAfBgNVHSME\n" \
+"GDAWgBRTeb9aqitKz1SA4dibwJ3ysgNmyzAdBgNVHQ4EFgQUs5Cn2MmvTs1hPJ98\n" \
+"rV1/Qf1pMOowDgYDVR0PAQH/BAQDAgGGMBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYD\n" \
+"VR0lBBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMCIGA1UdIAQbMBkwDQYLKwYBBAGy\n" \
+"MQECAhowCAYGZ4EMAQIBMFAGA1UdHwRJMEcwRaBDoEGGP2h0dHA6Ly9jcmwudXNl\n" \
+"cnRydXN0LmNvbS9VU0VSVHJ1c3RSU0FDZXJ0aWZpY2F0aW9uQXV0aG9yaXR5LmNy\n" \
+"bDB2BggrBgEFBQcBAQRqMGgwPwYIKwYBBQUHMAKGM2h0dHA6Ly9jcnQudXNlcnRy\n" \
+"dXN0LmNvbS9VU0VSVHJ1c3RSU0FBZGRUcnVzdENBLmNydDAlBggrBgEFBQcwAYYZ\n" \
+"aHR0cDovL29jc3AudXNlcnRydXN0LmNvbTANBgkqhkiG9w0BAQwFAAOCAgEAWGf9\n" \
+"crJq13xhlhl+2UNG0SZ9yFP6ZrBrLafTqlb3OojQO3LJUP33WbKqaPWMcwO7lWUX\n" \
+"zi8c3ZgTopHJ7qFAbjyY1lzzsiI8Le4bpOHeICQW8owRc5E69vrOJAKHypPstLbI\n" \
+"FhfFcvwnQPYT/pOmnVHvPCvYd1ebjGU6NSU2t7WKY28HJ5OxYI2A25bUeo8tqxyI\n" \
+"yW5+1mUfr13KFj8oRtygNeX56eXVlogMT8a3d2dIhCe2H7Bo26y/d7CQuKLJHDJd\n" \
+"ArolQ4FCR7vY4Y8MDEZf7kYzawMUgtN+zY+vkNaOJH1AQrRqahfGlZfh8jjNp+20\n" \
+"J0CT33KpuMZmYzc4ZCIwojvxuch7yPspOqsactIGEk72gtQjbz7Dk+XYtsDe3CMW\n" \
+"1hMwt6CaDixVBgBwAc/qOR2A24j3pSC4W/0xJmmPLQphgzpHphNULB7j7UTKvGof\n" \
+"KA5R2d4On3XNDgOVyvnFqSot/kGkoUeuDcL5OWYzSlvhhChZbH2UF3bkRYKtcCD9\n" \
+"0m9jqNf6oDP6N8v3smWe2lBvP+Sn845dWDKXcCMu5/3EFZucJ48y7RetWIExKREa\n" \
+"m9T8bJUox04FB6b9HbwZ4ui3uRGKLXASUoWNjDNKD/yZkuBjcNqllEdjB+dYxzFf\n" \
+"BT02Vf6Dsuimrdfp5gJ0iHRc2jTbkNJtUQoj1iM=\n" \
+"-----END CERTIFICATE-----\n";
+
+WiFiMulti WiFiMulti;
+
+#define LED_PIN 15 // strip is connected to pin
+#define LED_COUNT 12 // number of pixels
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+#define HIGH_CO2_BOUNDARY 2000
+#define LOW_CO2_BOUNDARY 1000
+
+//Adafruit_BME680 bme; // I2C
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+static int64_t lastMmntTime = 0;
+static int startCheckingAfterUs = 1900000;
+
+float hum_weighting = 0.25; // so hum effect is 25% of the total air quality score
+float gas_weighting = 0.75; // so gas effect is 75% of the total air quality score
+
+float hum_score, gas_score;
+float gas_reference = 250000;
+float hum_reference = 40;
+int getgasreference_count = 0;
+int ppm;
+int hum;
+int temp;
+
+struct Button {
+  const uint8_t PIN;
+  uint32_t numberKeyPresses;
+  bool pressed;
+};
+
+Button button1 = {0, 0, false};
+
+void IRAM_ATTR isr() {
+  button1.numberKeyPresses += 1;
+  button1.pressed = true;
+}
+
+// Setting clock via ntp...
+void setClock() {
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print(F("Waiting for NTP time sync: "));
+  time_t nowSecs = time(nullptr);
+  while (nowSecs < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(F("."));
+    yield();
+    nowSecs = time(nullptr);
+  }
+
+  Serial.println();
+  struct tm timeinfo;
+  gmtime_r(&nowSecs, &timeinfo);
+  Serial.print(F("Current time: "));
+  Serial.print(asctime(&timeinfo));
+}
+
+void setup()
+{
+  Serial.begin(115200);
+ 
+  Wire.begin();
+  delay(1000); // give sensors some time to power-up
+
+  attachInterrupt(button1.PIN, isr, FALLING);  
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("<ssid>", "<passwort>");
+
+  // wait for WiFi connection
+  Serial.print("Waiting for WiFi to connect...");
+  while ((WiFiMulti.run() != WL_CONNECTED)) {
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  setClock();  
+  
+  if (airSensor.begin() == false)
+  {
+    Serial.println("Air sensor not detected. Please check wiring. Freezing...");
+    while (1)
+      ;
+  }  
+/*
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
+*/
+  strip.begin();
+  strip.setBrightness(64); // set brightness
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop()
+{
+  
+  if (airSensor.dataAvailable())
+  {
+      /*
+    Serial.print("co2(ppm):");
+    Serial.print(airSensor.getCO2());
+
+    Serial.print(" temp(C):");
+    Serial.print(airSensor.getTemperature(), 1);
+
+    Serial.print(" humidity(%):");
+    Serial.print(airSensor.getHumidity(), 1);
+  */
+    ppm=airSensor.getCO2();
+    hum=airSensor.getHumidity();
+    temp=airSensor.getTemperature();
+  } 
+  
+  if (button1.pressed) {
+    Serial.printf("Button 1 has been pressed %u times\n", button1.numberKeyPresses);
+    button1.pressed = false;
+
+    // calibration
+    airSensor.setAltitudeCompensation(0); // Altitude in m ü NN
+    airSensor.setForcedRecalibrationFactor(400); // fresh air
+    Serial.print("SCD30 calibration, please wait 30 s ...");
+      
+    strip.clear();
+    for ( int i = 0; i < LED_COUNT; i++) { // blue for calibration
+      strip.setPixelColor(i, 0, 0, 255);
+    }
+    strip.show();
+      
+    delay(30000);
+      
+    Serial.println("Calibration done");
+    Serial.println("Rebooting device");
+    ESP.restart();
+      
+    delay(5000);
+  }
+
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if(client) {
+    client -> setCACert(rootCACertificate);
+
+    {
+      // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+      HTTPClient https;
+  
+      //Serial.print("[HTTPS] begin...\n");
+
+      // add device id and authentication
+      https.begin("https://element-iot.ch/api/v1/devices/<deviceid>/packets?auth=<authorization>");
+
+      // convert ppm to two bytes in hex, with leading 0 and uppercase
+      String str1 = String(highByte(ppm), HEX);
+      if (str1.length()==1) { str1="0" + str1;};
+      str1.toUpperCase();
+      String str2 = String(lowByte(ppm), HEX);
+      if (str2.length()==1) { str2="0" + str2;};
+      str2.toUpperCase();
+
+      // submit payload via POST, as json
+      https.addHeader("Content-Type", "application/json");
+      String payload="{\"packet\":{\"payload_encoding\":\"binary\",\"payload\":\"";
+      payload +=str1;
+      payload +=str2;
+      payload +="\"}}";
+
+      //Serial.println(payload);
+      
+      int httpResponseCode = https.POST(payload);
+   
+        // httpCode will be negative on error
+        if (httpResponseCode > 0) {
+          // HTTP header has been send and Server response header has been handled
+          //Serial.printf("[HTTPS] POST... code: %d\n", httpResponseCode);
+  
+        } else {
+          Serial.printf("[HTTPS] POST... failed, error: %s\n", https.errorToString(httpResponseCode).c_str());
+        }
+  
+        https.end();
+      }
+  
+    delete client;
+  } else {
+    Serial.println("Unable to create client");
+  }
+
+/*
+if (! bme.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bme.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.humidity);
+  Serial.println(" %");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("        Gas = ");
+  Serial.print(bme.readGas());
+  Serial.println("R\n");
+
+   //Calculate humidity contribution to IAQ index
+  float current_humidity = bme.readHumidity();
+  if (current_humidity >= 38 && current_humidity <= 42)
+    hum_score = 0.25*100; // Humidity +/-5% around optimum 
+  else
+  { //sub-optimal
+    if (current_humidity < 38) 
+      hum_score = 0.25/hum_reference*current_humidity*100;
+    else
+    {
+      hum_score = ((-0.25/(100-hum_reference)*current_humidity)+0.416666)*100;
+    }
+  }
+  
+  //Calculate gas contribution to IAQ index
+  float gas_lower_limit = 5000;   // Bad air quality limit
+  float gas_upper_limit = 50000;  // Good air quality limit 
+  if (gas_reference > gas_upper_limit) gas_reference = gas_upper_limit; 
+  if (gas_reference < gas_lower_limit) gas_reference = gas_lower_limit;
+  gas_score = (0.75/(gas_upper_limit-gas_lower_limit)*gas_reference -(gas_lower_limit*(0.75/(gas_upper_limit-gas_lower_limit))))*100;
+  
+  //Combine results for the final IAQ index value (0-100% where 100% is good quality air)
+  float air_quality_score = hum_score + gas_score;
+
+  Serial.println("Air Quality = "+String(air_quality_score,1)+"% derived from 25% of Humidity reading and 75% of Gas reading - 100% is good quality air");
+  Serial.println("Humidity element was : "+String(hum_score/100)+" of 0.25");
+  Serial.println("     Gas element was : "+String(gas_score/100)+" of 0.75");
+  if (bme.readGas() < 120000) Serial.println("***** Poor air quality *****");
+  Serial.println();
+  if ((getgasreference_count++)%10==0) GetGasReference(); 
+  Serial.println(CalculateIAQ(air_quality_score));
+  Serial.println("------------------------------------------------");
+*/
+    int c;
+
+    //int qs=air_quality_score;
+    //int qs=161;
+
+    //if (qs>200) { c=strip.Color(255,0,0); } // red
+    //if (qs>150 && qs<200) { c=strip.Color(255,165,0); } // yellow
+    //if (qs<150) { c=strip.Color(0,128,0); } // green
+    
+    if (ppm>HIGH_CO2_BOUNDARY) { c=strip.Color(255,0,0); }
+    if (ppm>LOW_CO2_BOUNDARY && ppm <= HIGH_CO2_BOUNDARY) { c=strip.Color(255,165,0); }
+    if (ppm<=LOW_CO2_BOUNDARY) { c=strip.Color(0,128,0); }    
+
+    int ct=map(ppm, 350, HIGH_CO2_BOUNDARY, 1, 12);
+    //int ct=map(qs, 0, 300, 12, 1);
+  
+    for(uint16_t i=0; i<strip.numPixels(); i++) {
+      if (i<=ct) {
+        strip.setPixelColor(i, c);
+      } else {
+        strip.setPixelColor(i, strip.Color(0,0,0));
+      }
+    }
+    strip.show();    
+    
+    delay(2000);
+}
+
+ /*void GetGasReference(){
+  // Now run the sensor for a burn-in period, then use combination of relative humidity and gas resistance to estimate indoor air quality as a percentage.
+  Serial.println("Getting a new gas reference value");
+  int readings = 10;
+  for (int i = 1; i <= readings; i++){ // read gas for 10 x 0.150mS = 1.5secs
+    gas_reference += bme.readGas();
+  }
+  gas_reference = gas_reference / readings;
+}*/
+
+String CalculateIAQ(float score){
+  String IAQ_text = "Air quality is ";
+  score = (100-score)*5;
+  if      (score >= 301)                  IAQ_text += "Hazardous";
+  else if (score >= 201 && score <= 300 ) IAQ_text += "Very Unhealthy";
+  else if (score >= 176 && score <= 200 ) IAQ_text += "Unhealthy";
+  else if (score >= 151 && score <= 175 ) IAQ_text += "Unhealthy for Sensitive Groups";
+  else if (score >=  51 && score <= 150 ) IAQ_text += "Moderate";
+  else if (score >=  00 && score <=  50 ) IAQ_text += "Good";
+  return IAQ_text;
+}
